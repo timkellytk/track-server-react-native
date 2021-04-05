bf033c14d3b42c13e82b3b28707f4ceec5842837
Tim Kelly <timpkelly1@gmail.com>
2021-04-05 12:42:10 +1000

    Finished Section 14: Building a Custom Express API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,37 @@
-const express = require("express");
+require('./models/User');
+require('./models/Track');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
+const trackRoutes = require('./routes/trackRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.use(bodyParser.json());
+app.use(authRoutes);
+app.use(trackRoutes);
+
+const mongoUri =
+  'mongodb+srv://timtest:passwordpassword@nativecluster.eosvt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on('connected', () => {
+  console.log('connected to mongo instance');
+});
+mongoose.connection.on('error', (err) => {
+  console.error('error when connecting to mongo instance', err);
+});
+
+app.get('/', requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
